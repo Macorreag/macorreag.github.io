48e66ebc1d34fd6a0542af49a427ede849b94b03
Miller Correa <macorreag@unal.edu.co>
2020-11-03 00:21:53 -0500

    add metadata to project config

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,14 +5,19 @@
  */
 
 module.exports = {
+  siteMetadata: {
+    title: 'Miller Correa',
+    description: 'Pagina Web principal de mis estudios y demás cosas que desarrollo en la Web.',
+    author: '@macorreag',
+  },
   /* Your site config here */
   plugins: [
-    "gatsby-plugin-postcss",
-    "gatsby-transformer-json",
+    'gatsby-plugin-postcss',
+    'gatsby-transformer-json',
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: "data",
+        name: 'data',
         path: `${__dirname}/src/data`,
       },
     },
